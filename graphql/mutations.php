d9e028ba1705ce587e59fc193946a59886ef01f8
Pierre Cabrière <hello@pierrecabriere.fr>
2018-02-08 02:16:51 +0100

    change email background color

--- graphql/mutations.php
+++ graphql/mutations.php
@@ -49,7 +49,7 @@ function wp_contact_mutation($fields) {
 
 			$input['message'] = nl2br($input['message']);
 
-			sleep(2);
+			sleep(4);
 
 			// notification email sent to admin
 			$to = get_bloginfo('admin_email');
