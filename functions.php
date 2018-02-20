b911ba0b710191ac4edf5df965893ce016467090
Pierre Cabrière <hello@pierrecabriere.fr>
2018-02-20 17:45:02 +0100

    fix tests

--- functions.php
+++ functions.php
@@ -1,21 +1,5 @@
 <?php
 
-use GraphQL\Error\Error as GraphQLError;
-
 //require_once 'configuration/index.php';
 //require_once 'types/index.php';
 require_once 'graphql/index.php';
-
-function wp_custom_fields_project($fields) {
-
-	$user_logged_in = is_user_logged_in();
-
-//	if ( !$user_logged_in ) {
-	$fields['title']['resolve'] = function()  { throw new GraphQLError('Can\'t access this title.'); };
-//	}
-
-	return $fields;
-
-}
-
-add_filter('graphql_post_fields', 'wp_custom_fields_project', 11);
