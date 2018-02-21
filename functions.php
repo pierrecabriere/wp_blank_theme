97c805b545127da43bffd22ecca1e8bcd183d6b0
Pierre Cabrière <hello@pierrecabriere.fr>
2018-02-22 00:13:01 +0100

    test

--- functions.php
+++ functions.php
@@ -1,5 +1,21 @@
 <?php
 
+use GraphQL\Error\UserError;
+
 //require_once 'configuration/index.php';
 //require_once 'types/index.php';
 require_once 'graphql/index.php';
+
+function wp_custom_fields_project($fields) {
+
+//	$user_logged_in = is_user_logged_in();
+
+//	if ( !$user_logged_in ) {
+	$fields['title']['resolve'] = function()  { throw new UserError('Can\'t access this title.'); };
+//	}
+
+	return $fields;
+
+}
+
+add_filter('graphql_post_fields', 'wp_custom_fields_project', 11);
