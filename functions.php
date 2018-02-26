708078e10351c4bacc2ded3963568951eec33135
Pierre Cabrière <hello@pierrecabriere.fr>
2018-02-26 21:22:47 +0100

    update jwt auth expire

--- functions.php
+++ functions.php
@@ -1,5 +1,19 @@
 <?php
 
+use GraphQL\Error\Error as GraphQLError;
+
 //require_once 'configuration/index.php';
 //require_once 'types/index.php';
 require_once 'graphql/index.php';
+
+
+function wp_custom_fields_project($fields) {
+	$user_logged_in = is_user_logged_in();
+
+	if (!$user_logged_in)
+		$fields['title']['resolve'] = function() { return 'Please login'; };
+
+	return $fields;
+}
+
+add_filter('graphql_post_fields', 'wp_custom_fields_project', 11);
